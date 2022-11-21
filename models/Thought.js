0179ea341141ceb524c288f18734e64d356446ea
ckrum21 <108544020+ckrum21@users.noreply.github.com>
2022-11-21 14:01:48 -0500

    reation schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require('mongoose');
 
+const moment = require('moment');
+
 const thoughtSchema = new Schema(
     {
         thoughtText: {
@@ -25,6 +27,42 @@ const thoughtSchema = new Schema(
         },
     }
 );
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 180,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtDate => moment(createdAtDate).format("MMM DD, YYYY [at] hh:mm a"),
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount')
+.get(function() {
+    return this.reactions.length;
+});
+
 
 const Thought = model('thought', thoughtSchema);
 
